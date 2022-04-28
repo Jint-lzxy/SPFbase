c2a9f9034924fd46c4177d99a4d46bbc45fdd679
Jint-lzxy <WinnieJt20050921@126.com>
2022-04-28 16:44:58 +0800

    Clean Update. Merged all past results and added this new one.

---
+++ Eyes2D/dllmain.cpp
@@ -0,0 +1,22 @@
+﻿#include "E2DLogSystem.hpp"
+#include <Windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	using namespace Eyes2D;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		EYESDEBUG(L"Eyes2D.dll process attach.");
+		break;
+	case DLL_PROCESS_DETACH:
+		EYESDEBUG(L"Eyes2D.dll process detach.");
+		break;
+	case DLL_THREAD_ATTACH:
+		EYESDEBUG(L"Eyes2D.dll thread attach.");
+		break;
+	case DLL_THREAD_DETACH:
+		EYESDEBUG(L"Eyes2D.dll thread detach.");
+		break;
+	}
+	return TRUE;
+}
